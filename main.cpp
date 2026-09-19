5496c47f1d557ce2e134bf89d4778e130a3b2f47
HuiluanYie <huiluanyie@gmail.com>
2026-09-18 23:43:33 -0700

    Commit 2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 #include <string>
 using namespace std;
 
+const int NR_ORDER = 3;
+
 struct Order
 {
+    // an order includes a customer, the number of items in it, and an array of items
     string customer;
     int nr_item;
     string* items;
@@ -34,7 +37,22 @@ int main() {
 //Function definition
 void input_order(Order* o)
 {
-    cout << ""
+    cout << "Please enter customer name: ";
+    cin >> o -> customer;
+    cout << "Please enter the number of items in this order: ";
+    cin >> o -> nr_item;
+
+    int size = o -> nr_item;
+    o -> items = new string [size];
+
+    cout << "Please enter the items in this order: ";
+    for (int i = 0; i < size; i++)
+    {
+        
+    }
+    
+    cout << "Please enter the number of items in this order: ";
+    cin >> o->nr_item;
 }
 
 void display_order(Order* o)
