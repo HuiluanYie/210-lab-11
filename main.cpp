5b37dae81bb700f7053ba6d84d34a38baee41215
HuiluanYie <huiluanyie@gmail.com>
2026-09-18 23:33:13 -0700

    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
+// COMSC-210 | Lab 11 | Huiluan Yie
+
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct Order
+{
+    string customer;
+    int nr_item;
+    string* items;
+
+    ~Order()
+    {
+        if (items)
+        {
+            delete [] items;
+        }
+        items = nullptr;
+    }
+};
+
+//Function prototype
+void input_order(Order*);
+void display_order(Order*);
+
 int main() {
-    
+    // declarations
+
     return 0;
+}
+
+
+//Function definition
+void input_order(Order* o)
+{
+    cout << ""
+}
+
+void display_order(Order* o)
+{
+
 }
