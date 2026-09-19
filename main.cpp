20d73270a1d7a5d291a61d21258998eefc077c81
HuiluanYie <huiluanyie@gmail.com>
2026-09-19 00:04:05 -0700

    Commit 4

--- main.cpp
+++ main.cpp
@@ -46,9 +46,16 @@ void input_order(Order* o)
 {
     cout << "\nPlease enter customer name: ";
     getline(cin, o->customer);
+
     cout << "Please enter the number of items in this order: ";
     int size;
     cin >> size;
+    while (size < 1) // input validate
+    {
+        cout << "Invalid number of items (at least 1 item in the order);\n";
+        cout << "Please enter the correct number of items: ";
+        cin >> size;
+    }
     cin.ignore(); // handle extra \n
     o->nr_item = size;
 
@@ -63,5 +70,9 @@ void input_order(Order* o)
 
 void display_order(Order* o)
 {
-
+    cout << "Hi " << o->customer << ", here's your list of items in your order: \n";
+    for (int i = 0; i < o->nr_item; i++)
+    {
+        cout << "Item #" << i + 1 << ": ";
+    }
 }
